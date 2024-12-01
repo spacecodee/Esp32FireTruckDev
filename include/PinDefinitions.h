653a4d1069b14ab3b7aca205209660e31d340d1e
spacecodee <spacecodee@gmail.com>
2024-12-01 11:53:16 -0500

    Update PinDefinitions.h to improve pin documentation and change pin assignments: clarify pin capabilities for servo, LED, pump, motor driver, and flame sensors.

--- include/PinDefinitions.h
+++ include/PinDefinitions.h
@@ -2,32 +2,32 @@
 #define PIN_DEFINITIONS_H
 
 // Servo configuration
-#define MY_SERVO_PIN 13  // PWM capable
+#define MY_SERVO_PIN 13  // Good - I/O capable, ADC2_CH4
 
 // LED Pins
-#define LED_RED 18    // GPIO18
-#define LED_GREEN 19  // GPIO19
-#define LED_PIN 2     // Built-in LED
+#define LED_RED 18    // Good - I/O capable
+#define LED_GREEN 19  // Good - I/O capable
+#define LED_PIN 2     // Good - Built-in LED
 
 // Pump configuration
-#define PUMP_PIN 22  // PWM capable
+#define PUMP_PIN 22  // Good - I/O capable
 #define PUMP_FREQUENCY 5000
 #define PUMP_CHANNEL 0
 #define PUMP_RESOLUTION 8
 
 // L298N Motor Driver pins
-#define ENA 36  // Changed to ADC1_0
-#define ENB 39  // Changed to ADC1_3
-#define IN1 27  // Keep existing
-#define IN2 14  // Keep existing
-#define IN3 12  // Keep existing
-#define IN4 15  // Keep existing
+#define ENA 5   // Changed to GPIO5 - I/O capable
+#define ENB 17  // Changed to GPIO17 - I/O capable
+#define IN1 27  // Good - I/O capable
+#define IN2 14  // Good - I/O capable
+#define IN3 12  // Good - I/O capable
+#define IN4 15  // Good - I/O capable
 #define MOTOR_SPEED 200
 
-// Flame sensors (ADC1 pins)
-#define FLAME_SENSOR_1 34  // ADC1_6
-#define FLAME_SENSOR_2 35  // ADC1_7
-#define FLAME_SENSOR_3 32  // ADC1_4
+// Flame sensors (ADC1 pins - Input only)
+#define FLAME_SENSOR_1 34  // Good - Input only ADC1_CH6
+#define FLAME_SENSOR_2 35  // Good - Input only ADC1_CH7
+#define FLAME_SENSOR_3 32  // Good - I/O capable ADC1_CH4
 
 // ADC configuration
 #define ADC_RESOLUTION 4095  // Updated for ESP32 12-bit ADC
